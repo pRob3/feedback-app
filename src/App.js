899c52c395b5fb5971bbfe815f70287941f7e31b
pRob3 <skene@hotmail.com>
2022-01-12 07:27:17 +0100

    Feedback form and simple animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { v4 as uuidv4} from 'uuid'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+
 import FeedbackData from './data/FeedbackData'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    // console.log(newFeedback)
+
+    // Add the feedback to the old ones
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     // console.log('App', id)
@@ -18,6 +29,7 @@ function App() {
     <>
       <Header />
       <div className='container'>
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
