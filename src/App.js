0416c9b6fafc9f577e79a0f2b23a1f3d33401173
pRob3 <bubblan@slaf.se>
2022-03-04 08:01:22 +0100

    Created app context and provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { v4 as uuidv4} from 'uuid'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { useState } from 'react'
+
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
-
 import AboutIconLink from './components/AboutIconLink'
 
 import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './context/FeedbackContext'
 
 import FeedbackData from './data/FeedbackData'
 
@@ -31,27 +32,29 @@ function App() {
   }
 
   return (
-    <Router>
-      <Header />
-      <div className='container'>
-        <Routes>
-          <Route exact path='/' element={
-            <>
-              <FeedbackForm handleAdd={addFeedback} />
-              <FeedbackStats feedback={feedback} />
-              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-            </>
-          }>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route exact path='/' element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+              </>
+            }>
 
-          </Route>
+            </Route>
 
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
 
 
-          <AboutIconLink />
-      </div>
-    </Router>
+            <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   )
 }
 
